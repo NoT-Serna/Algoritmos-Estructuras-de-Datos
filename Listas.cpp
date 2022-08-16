e3f52b4c0ad8575daaf9800ffe0af29a85d866d7
Serno uwu <jserna7856@gmail.com>
2022-08-16 09:42:47 -0500

    New linked lists methods

--- Listas.cpp
+++ Listas.cpp
@@ -1,8 +1,7 @@
-
-// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
-
 #include<iostream>
 #include <math.h> 
+#include <stdexcept>
+#include <string>
 
 using namespace std;
 
@@ -70,15 +69,6 @@ public:
 		setY(f.y);
 		return *this; 
 	}
-	
-	string to_string() {
-		return "("+std::to_string(x) + "," + std::to_string(y)+")";
-	}
-
-
-	friend std::ostream& operator<<(std::ostream& os, Point& b) {
-		return os << b.to_string();
-	}
 
 };
 
@@ -118,20 +108,14 @@ public:
   void setNext(Nodo* p){
       pointer = p;
   }
-    
-    string to_string() {
-		return getDato()->to_string();
-	}
-
-
-	friend std::ostream& operator<<(std::ostream& os, Nodo& b) {
-		return os << b.to_string();
-	}
+  
+  void setNext() {
+      pointer = NULL;
+  }
     
 };
 
 class Lista{
-    
     Nodo* ptr;
     int size;
 
@@ -144,13 +128,12 @@ public:
     
     ~Lista(){
         Nodo* t = ptr;
-        Nodo* n;
+        Nodo * n;
         while(t->getNext() != NULL){
             n = t;
-            t = t->getNext();    
+            t = t->getNext();
             delete n;
         }
-        delete t;
     }
     
     void push_back(Point* d){
@@ -173,68 +156,102 @@ public:
         return size;
     }
     
+    void setPtr(Nodo* ptrr) {
+        ptr = ptrr;
+    }
+    
+     Nodo* getNodo(int pos) {
+        if(size == 0) {
+            throw invalid_argument("La lista no tienen nodos");
+        } else if (pos >= size || pos < 0) {
+            throw invalid_argument("El nodo no esta en la lista");
+        } else {
+            Nodo* t = ptr;
+            for(int i = 0; i < pos; i++) {
+                t = t->getNext();
+            }
+            return t;
+        }
+    }
+    
+    void insert(Point* d, int pos) {
+        if(pos > size || pos < 0) {
+            throw invalid_argument("No es posible insertar un nodo en esa posicion");
+        } else if(pos == 0) {
+            Nodo* r = getNodo(pos);
+            Nodo* n = new Nodo(d);
+            n->setNext(r);
+            ptr = n;
+            size++;
+        } else if(pos == size || size == 0) {
+            push_back(d);
+        } else {
+            Nodo* l = getNodo(pos-1);
+            Nodo* r = l->getNext();
+            
+            Nodo* n = new Nodo(d);
+            l->setNext(n);
+            n->setNext(r);
+            size++;
+        }
+    }
+    
+    
+    void remove_nodo(int pos){
+        if (pos < 0 || pos >= size) {
+            throw invalid_argument("No hay ningun nodo que eliminar en esa posicion");
+        } else if(pos == 0) {
+            Nodo* obj = ptr;
+            ptr = obj->getNext();
+            delete obj;
+            size--;
+        } else if (pos == size - 1) {
+           Nodo* l = getNodo(pos-1);
+           Nodo* obj = l->getNext();
+           l->setNext();
+           delete obj;
+           size--;
+        } 
+        else {
+            Nodo* l = getNodo(pos-1);
+            Nodo* obj = l->getNext();
+            Nodo* r = obj->getNext();
+            
+            l->setNext(r);
+            delete obj;
+            size--;
+        }
+    }
+    
+    
+    void reverse(){
+        for(int i = 1;i<size;i++){
+            Nodo* l = getNodo(i-1);
+            //cout << l->getDato()->getX();
+            cout<< i;
+            Nodo* n = l->getNext();
+            ptr = n;
+           //Nodo* r = ptr;
+            l->setNext(n->getNext());
+            n->setNext(l);
+            print();
+            
+            // usar insert cada VEZ EN PUESTO +1
+        }
+    }
+    
     void print(){
         if(size == 0){
-            cout<<"La lista está vacía"<<endl;
+            cout<<"La lista está vacía" <<endl;
         }else{
             Nodo* t = ptr;
             do{
-                //cout<<"("<<(*t).getDato()->getX()<<", "<<t->getDato()->getY()<<"), ";
-                cout<<(*t)<<", ";
+                cout<<"("<<(*t).getDato()->getX()<<", "<<t->getDato()->getY()<<"), ";
                 t = t->getNext();
                 
             }while(t != NULL);
             cout<<endl;
         }
-    }
-    
-    Nodo* get(int i){
-        if(i < size && i>=0){
-            Nodo* n = ptr;
-            for(int x = 0; x<i;x++){
-                n = n->getNext();
-            }
-            return n;
-        }else{
-            //throw invalid_argument("La posicion no existe");
-            if(size == 0){
-                cout<<"La lista está vacía";
-            }else{
-                cout<<"La posicion no existe";
-            }
-            return NULL;
-        }
-        
-    }
-    
-    void insert(Point* p, int pos){
-        if(pos >= 0 && pos <= size){
-            //Si la lista está vacía o si se quiere insertar el nodo al final
-            //se usa el método push_back
-            if(size == 0 || pos == size){ 
-                push_back(p);
-            }else{
-                Nodo* n = new Nodo(p);
-                //Si se quiere insertar el nodo de primero en la lista
-                if(pos == 0){
-                    n->setNext(ptr);
-                    ptr = n;
-                }else{
-                    Nodo* t = get(pos-1);
-                    n->setNext(t->getNext());
-                    t->setNext(n);
-                }
-                size++;
-            }
-        }else{
-            throw invalid_argument("La posicion no existe");
-        }
-        
-    }
-    
-    void remove(int pos){
-        
-        
     }
     
 };
@@ -245,35 +262,13 @@ int main()
    
    Lista l = Lista();
    
-   l.push_back(new Point(1,1));
-   
-   l.print();
-   
-   l.push_back(new Point(2,2));
-   
-   l.print();
-   
-   for(int i = 3; i<10; i++){
+   // PUSH BACK
+   for(int i = 1; i<11; i++){
        l.push_back(new Point(i,i));
    }
-   
    l.print();
    
-   cout<<(*l.get(2)->getDato())<<endl;
-   cout<<(*l.get(2))<<endl;
+    
    
-   l.insert(new Point(20,20),5);
-   
-   l.print();
-   
-   Lista l2 = Lista();
-   
-   //l2.insert(new Point(20,20),5);
-   l2.insert(new Point(20,20),0);
-   l2.insert(new Point(19,19),0);
-   
-   l2.print();
-   
-   
-    return 0;
+   return 0;
 }
