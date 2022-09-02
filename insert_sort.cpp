506cf1b48644ab418f097efd4dc8e6ea6f4a25f5
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-09-02 08:13:56 -0500

    Update insert_sort.cpp

--- insert_sort.cpp
+++ insert_sort.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-
-int main(){
-    int A[10] = {1,3,5,7,9,8,5,2,4,6};
-    int i,j, temp;
-    for(i = 0; i<10; i++){
+int insert_sort(int A[10]){
+    int i, j , temp;
+    for(i = 0; i< 10; i++){
         temp = A[i];
-        j = i-1;
+        j = i -1;
 
         while(j>= 0 && A[j] > temp){
+            A[j+1] = A[j];
             j--;
         }
         A[j+1] = temp;
     }
-    for(int i = 0; i<10; i++){
-        cout <<A[i];
+    for(int i = 0; i<10 ; i++){
+        cout<< A[i];
     }
     return 0;
+}
+
+int main(){
+    int A[10] = {9,8,7,6,5,4,3,2,1};
+    insert_sort(A);
 }
