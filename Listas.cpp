9ea162bcf36c7c292f0fca9f93a42a6c80f5eeb3
Serno uwu <jserna7856@gmail.com>
2022-09-12 22:42:54 -0500

    test_1

--- Listas.cpp
+++ Listas.cpp
@@ -2,7 +2,6 @@
 #include <math.h> 
 #include <stdexcept>
 #include <string>
-
 using namespace std;
 
 class Point {
--- Talleres/test1.cpp
+++ Talleres/test1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+
+int main(){
+    vector<int> vec_1;
+    int myints[] = {1,2,3,4,5,6,7,8,9};
+
+    for(int i = 0; i<vec_1.size(); i++){
+        cout<<vec_1[i];
+    }
+    
+    vec_1.assign(myints,myints+9);
+    vec_1.push_back(vec_1.at(8)+1);
+    cout<<"The missing element in the vector is "<<vec_1.at(9);
+
+}
