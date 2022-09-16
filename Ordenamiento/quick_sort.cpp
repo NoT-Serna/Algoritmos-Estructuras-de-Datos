43e6683d1a2f015a43fe2afd909e2341b7f9f7bd
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-09-16 08:42:31 -0500

    Update quick_sort.cpp

--- Ordenamiento/quick_sort.cpp
+++ Ordenamiento/quick_sort.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- void partition(vector<int> vec_1, int ini, int fin){
-    int i = -1;
+int partition(vector<int>* vec_1, int ini, int fin){
+    int i = ini-1;
     int j= 0;
-    int pivot = vec_1.size()-1;
+    int pivot = vec_1->at(fin);
      
-    for(j = 0; j<vec_1.size(); j++){
-        if(pivot>=vec_1.at(j)){
+    for(j = ini; j<=fin; j++){
+        if(vec_1->at(j)<=pivot){
             i++;
-            int temp = vec_1.at(j++);
-            vec_1.at(j) = temp;
+            int temp = vec_1->at(j);
+            vec_1->at(j) = vec_1->at(i);
+            vec_1->at(i) = temp;
             
-        }else{
-            if(pivot<=vec_1.at(j)){
-                i++;
-                vec_1.at(j) = vec_1.at(j);
-            }
         }
     }
+    return i;
      
  }
  
-void quick_sort(){
-    
-}
+ void quick_sort(int* vec_1, int ini , int fin){
+     if(ini<fin){
+       int i = partition(&vec_1, ini, fin);
+         quick_sort(&vec_1, ini, i-1);
+         quick_sort(&vec_1, i+1, fin);
+     }
+ }
+ 
 
 int main() {
     
     int myints[] = {2,4,7,9,1,8,6,5,3};
     vector<int> vec_1;
     vec_1.assign(myints,myints+9);
-    partition(vec_1,2,3);
+    partition(&vec_1, 0, vec_1.size()-1);
+    
     for(int i = 0; i<vec_1.size(); i++){
-        cout<<vec_1.at(i);
-        
+        cout<<vec_1.at(i)<<" ";
     }
 
     return 0;
