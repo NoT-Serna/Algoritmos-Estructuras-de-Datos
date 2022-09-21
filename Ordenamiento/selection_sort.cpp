8accfa72f8110c6f845967af4554f7218ee147c0
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-09-21 16:31:10 -0500

    Update selection_sort.cpp

--- Ordenamiento/selection_sort.cpp
+++ Ordenamiento/selection_sort.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void selection_sort(int arr[]){
+    for (int i = 0; i<4; i++){
+        int min = i;
+    
+    for(int j = i+1; j<5; j++){
+        if (arr[j]< arr[min]){
+            min = j;
+        }
+    }
+    if (min != i){
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+  }
+}
+
+int main(){
+
+    int myarr[] = {3,8,2,1,4};
+    cout<< "Unsorted array: "<<endl;
+    for(int i = 0; i <5; i++){
+        cout<<myarr[i]<<" ";
+    }
+    cout<<endl;
+
+    selection_sort(myarr);
+    cout<<"Sorted Array: " <<endl;
+    for (int i = 0; i<5; i++){
+        cout<<myarr[i]<<" ";
+    }
+
+
+    return 0;
+}
