3ce1f34c0b14a71b39bb690dd8d6f4529fcef1cb
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-09-28 08:56:48 -0500

    Create implementacion.cpp

---
+++ Sets/implementacion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+using namespace std;
+// Conjuntos/Sets
+//Union
+
+int Union(set<int>* set_1, set<int>* set_2){
+    
+}
+
+
+int main() {
+    int myints[] = {30,20,15,32,5,10};
+    set<int> set_1 (myints,myints+6);
+    
+    int myints2[] = {31,21,16,33,6,11};
+    set<int> set_2 (myints,myints+6);
+    
+    
+
+    return 0;
+}  
