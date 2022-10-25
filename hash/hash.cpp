5ba3164ebd01451e77f7de87008d01762957cb07
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-10-24 21:23:34 -0500

    Add files via upload

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int sascii(string x, int M){
+    char ch[];
+    ch = x.toCharArray();
+    int xlength = x.length();
+
+    int i, sum;
+    for(sum=0, i = 0; i<x.length(); i++){
+        sum += ch[i];
+    }
+    return sum % M;
+}
+
+
+//Simple hash function//
+/*
+int main(){
+
+    return 0;
+    
+}
+
+#include <iostream>
+using namespace std;
+
+int h(int x) {
+    return x % 16;
+}
+
+int main(){
+    cout<<h(89);
+    return 0;
+}
+*/
