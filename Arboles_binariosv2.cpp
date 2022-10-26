f507c9b2773d25da2ffdfe7e572f33d7c43061ef
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2022-10-26 17:04:26 -0500

    Add files via upload

---
+++ Arboles_binariosv2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int idata){
+
+    Node* node = new Node();
+    node->data = idata;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int main(){
+
+    Node *root = newNode(1);
+    root->left = newNode(8);
+    root->right = newNode(7);
+    root->left->left = newNode(6);
+
+    cout<<root->left->left->data;
+    return 0;
+}
