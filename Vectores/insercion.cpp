e1f006416c6a2e4f323a28416fafc1eaf3bbf053
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2023-02-03 10:03:09 -0500

    Update insercion.cpp

--- Vectores/insercion.cpp
+++ Vectores/insercion.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void print(int v[], int tam){
+    for (int i = 0; i<tam; i++){
+        cout<<v[i]<<" ";
+    }
+}
+
+
 int insert_order(int x, int* v , int tam, int pos){
     for(int i = tam; i>pos; i--){
         v[i] = v[i-1];
@@ -10,23 +17,38 @@ int insert_order(int x, int* v , int tam, int pos){
     return tam +1;
 }
 
+bool search_num(int *v, int x, int tam){
+    for(int i = 0; i<tam; i++){
+        if(x == v[i]){
+            cout<<" Encontrado";
+            return true;
+        }
+        
+    }
+    cout<<"no fue encontrado";
+    return false;
+}
+    
+   
+
+
 
 int main(){
-   int tam = 7;
-   int vec_1[9];
+   int tam = 5;
+   int vec_1[tam];
+    for (int i = 0; i<tam; i++){
+        vec_1[i];
+    }
    
-   //Vector inicial 
-   for(int i = 0; i<tam; i++){
-        cout<<vec_1[i]<<"\t";
-   }    
+   //Vector inicial
+   print(vec_1,tam);
    cout<<"\n";
-
+    
    //Vector con insert
-   insert_order(100, vec_1, tam, 10 );
-   for(int i = 0; i<tam; i++){
-        cout<<vec_1[i]<<"\t";
-   }    
-   cout<<"\n";
+   insert_order(100, vec_1, tam, 1 );
+   print(vec_1,tam);
+   search_num(vec_1, 100, tam);
+   cout<<"\t";
    
     return 0;
 }
