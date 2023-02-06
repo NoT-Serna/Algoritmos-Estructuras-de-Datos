94885fa92949b99d72cd5335bdff8535621e58af
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2023-02-06 09:50:16 -0500

    Update Matriz_transpuesta.cpp

--- Matrices/Matriz_transpuesta.cpp
+++ Matrices/Matriz_transpuesta.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    
+    int m[5][5];
+    int c=5, f=5;
+    srand(1234);
+    
+    //Inicializar la matriz
+    for(int i = 0; i<f; i++){
+        for(int j =0; j<c; j++){
+            m[i][j] = rand()%100;
+        }
+    }
+    
+    //Imprmir la matriz
+    cout<<"Matriz original"<<"\n";
+    for(int i = 0; i<f; i++){
+        for(int j = 0; j<c; j++){
+            cout<<m[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    cout<<"\n";
+    
+    //Transpuesta de una matriz
+    cout<<"Matriz transpuesta"<<"\n";
+    for(int i = 0; i<f; i++){
+        for(int j = 0; j<c; j++){
+            cout<<m[j][i]<<"\t";
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    
+    return 0;
+}
