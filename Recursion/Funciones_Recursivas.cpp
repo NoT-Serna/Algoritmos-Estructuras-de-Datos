bb10c5b09cf07cff59b91528dfe348bfb07ecccb
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2023-05-10 10:11:50 -0500

    Update Funciones_Recursivas.cpp

--- Recursion/Funciones_Recursivas.cpp
+++ Recursion/Funciones_Recursivas.cpp
@@ -20,12 +20,39 @@ int fact(int x){
     }
 }
 
+int der(int i){
+
+    int der = 2*(i+1);
+    return der;
+}
+
+int izq(int i){
+    int izq = 2*(i+1)-1;
+    return izq;
+}
+
+void recorrido_recursivo(int vec[], int tam, int i){
+    if(izq(i) < tam){
+        recorrido(vec,tam,izq(i));
+    }
+    if(der(i)< tam){
+        recorrido(vec,tam,der(i));
+    }
+    cout<<vec[i]<<" ";
+}
+
 
 
 int main() {
     
     cout<<"Factorial "<<fact(3)<<endl;
     cout<<"Fibonacci "<<fibonacci(4);
+    
+    int tam = 9;
+    int vec[tam] = {7,2,4,9,8,3,1,5,6};
+    print(vec,tam);
+    cout<<endl;
+    recorrido_recursivo(vec,tam,0);
     
     return 0;
 }
