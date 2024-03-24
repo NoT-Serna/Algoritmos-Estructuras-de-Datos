6d5f09347efc5b4a57fe6a15d5bd084a95818eed
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2024-03-24 16:30:23 -0500

    Update Preparical 2_2024.cpp

--- Pruebas/Preparical 2_2024.cpp
+++ Pruebas/Preparical 2_2024.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 //Binary search
 //Problema 1
+/*
 double binary(int n, float x){
     int low  = 0;
     int high = n;
@@ -24,7 +25,7 @@ double binary(int n, float x){
         }
     }
   }
-  
+*/
   void print(vector<int>&v){
     for(int i = 0; i<10; i++){
         cout<<v.at(i)<<" ";
@@ -37,23 +38,37 @@ double binary(int n, float x){
 //Problema 2
 
 
-void bubble_sort(vector<int>&v){
-  int temp, j;
-  for(j = 0; j<v.size(); j++){
-    for(int i =0; i<v.size()-1; i++){
-        if(v.at(i) > v.at(i+1)){
-            temp = v.at(i);
-            v.at(i) = v.at(i+1);
-            v.at(i) = temp;
+void bubble_sort(vector<int>& v) {
+    int temp, j;
+    for (j = 0; j < v.size(); j++) {
+        for (int i = 0; i < v.size() - 1; i++) {
+            if (v.at(i) > v.at(i + 1)) {
+                temp = v.at(i);
+                v.at(i) = v.at(i + 1);
+                v.at(i + 1) = temp; // Corrected assignment here
+            }
         }
     }
-  }
+}
+
+void PairSum(vector<int>&v , int obj){
+    vector<pair<int,int>> pairs;
+    for(size_t i = 0; i < v.size(); i++){
+        for(size_t j = i+1; j< v.size(); j++){
+            if(v.at(i) + v.at(j) == obj){
+                pairs.push_back(make_pair(v.at(i), v.at(j)));
+            }
+        }
+    }
+    for(const auto& pair: pairs){
+        cout<< "(" << pair.first << ", "<< pair.second <<")"<< endl;
+    }
 }
 
 
-int Sum(vector<int>& v, int obj){
-  
-}
+
+
+
 
 int main() {
     
@@ -67,8 +82,9 @@ int main() {
     print(vec);
     
     cout<<endl;
-    
+    //Problema 2
     bubble_sort(vec);
+    PairSum(vec,8);
     print(vec);
     
     
